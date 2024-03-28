3f8fca8b83507fc984b23bb9cde2ede54de2956d
Mutisostud02 <https://github.com/Mutisostud02>
2024-03-28 12:57:55 +0300

    Added the functions to toggle the loading,first and third classes on and off Adde the settimeout function to display loading like screen when user presses view button

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
 const weatherSearch = document.querySelector('.weather');
 const input = document.querySelector('input');
 input.setAttribute('placeholder','enter location...');
-const svg = document.querySelector('svg');
-
+const button = document.querySelector('button');
+const img = document.querySelector('img');
+const html = document.querySelector('html');
+const loader = document.getElementById('loader');
+const first = document.getElementById('first');
+const third = document.getElementById('third');
 
 async function fetchData() {
+   try {
     const data = await fetch(`http://api.weatherapi.com/v1/current.json?key=504c5cfb26384fae8fe93859240602&q=${input.value}`);
     const response = await data.json();
     const result = response.current.condition.text;
-    weatherSearch.textContent = result
+    weatherSearch.value = result;
+    weatherSearch.innerHTML = `The weather at ${input.value} is ${result}`;
+   } catch(err) {
+    alert(err);
+   }
 }
-// async function weatherGifs() {
-//     console.log(weatherSearch.textContent)
-    // const data = await fetch(`api.giphy.com/v1/gifs/${weatherSearch.value}?api_key=MVyTaUBIpcZn7pon7F7ZDUgZDNS6XHxW`);
-    // const response = await data.json();
-    // console.log(response);
-// }
-svg.addEventListener('click', function (event) {
-    fetchData()
-    weatherGifs()
-    console.log(fetchData())
-})
+async function weatherGifs() {
+    if(weatherSearch.value != undefined){
+    const data = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=MVyTaUBIpcZn7pon7F7ZDUgZDNS6XHxW&s=${weatherSearch.value}&limit=1`);
+    const response = await data.json();
+    const result = response.data.images.original.url
+    img.src = result;
+    }    
+}
+loader.classList.toggle('loading');
+first.classList.toggle('first');
+third.classList.toggle('third')
 
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    html.setAttribute('style','opacity:0.5;')
+    loader.classList.toggle('loading');
+    first.classList.toggle('first');
+    third.classList.toggle('third')
+    console.log('hey now')
+    setTimeout(() => {
+        loader.classList.toggle('loading');
+        first.classList.toggle('first');
+        third.classList.toggle('third')
+        fetchData()
+        weatherGifs()
+        console.log('hey later')
+        html.removeAttribute('style','opacity:0.5;');
+    }, 3000);
+  
+ 
+   
+
+})
